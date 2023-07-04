8b969f9584415d1aded186e68058a7b35d67a5b4
Yoohaeun <haspring@dsm.hs.kr>
2023-07-04 21:44:58 +0900

    UserRepository

--- src/main/java/com/example/hackathon/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/hackathon/domain/user/repository/UserRepository.java
@@ -4,6 +4,10 @@ import com.example.hackathon.domain.user.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+<<<<<<< Updated upstream
+=======
+import javax.swing.text.html.Option;
+>>>>>>> Stashed changes
 import java.util.Optional;
 
 @Repository
