654ebff0e8586182e7634a10a4578a6c40778874
Yoohaeun <haspring@dsm.hs.kr>
2023-07-04 15:54:13 +0900

    회원가입 Request

---
+++ src/main/java/com/example/hackathon/domain/user/repository/dto/SignUpRequest.java
@@ -0,0 +1,26 @@
+package com.example.hackathon.domain.user.repository.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SignUpRequest {
+
+    @NotBlank
+    @Size(max = 11, message = "11자 까지 입력 가능합니다")
+    private String phoneNumber;
+
+    @NotBlank
+    @Size(max = 5, message = "5자 까지 입력 가능합니다")
+    private String guardianName;
+
+    @NotBlank
+    @Size(max = 11, message = "11자 까지 입력 가능합니다")
+    private String guardianPhoneNumber;
+
+    @NotBlank
+    private String deviceToken;
+}
