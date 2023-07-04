ea4933bf812ab1001756dfc72515718d125e9df4
Yoohaeun <haspring@dsm.hs.kr>
2023-07-04 15:54:49 +0900

    User Repository

---
+++ src/main/java/com/example/hackathon/domain/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.hackathon.domain.user.repository;
+
+import com.example.hackathon.domain.user.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    boolean existsByPhoneNumber(String phoneNumber);
+}
