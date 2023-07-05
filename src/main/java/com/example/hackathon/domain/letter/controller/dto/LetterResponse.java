10a34fdbc54f9d765d51230bbe75d7e2cf979692
Yoohaeun <haspring@dsm.hs.kr>
2023-07-05 19:14:05 +0900

    LetterResponse

---
+++ src/main/java/com/example/hackathon/domain/letter/controller/dto/LetterResponse.java
@@ -0,0 +1,10 @@
+package com.example.hackathon.domain.letter.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LetterResponse {
+    private final String wording;
+}
