a9e67bffcd277bb83e9c007f5d90e750dca9f073
Yoohaeun <haspring@dsm.hs.kr>
2023-07-05 15:49:33 +0900

    WhetherMealController

--- src/main/java/com/example/hackathon/domain/whether/controller/WhetherMealController.java
+++ src/main/java/com/example/hackathon/domain/whether/controller/WhetherMealController.java
@@ -1,12 +1,22 @@
 package com.example.hackathon.domain.whether.controller;
 
+import com.example.hackathon.domain.whether.controller.dto.WhetherMealRequest;
 import com.example.hackathon.domain.whether.service.WhetherMealService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
+@RequestMapping("/whether")
 @RestController
 public class WhetherMealController {
 
     private final WhetherMealService whetherMealService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/{id}")
+    public void createWhetherMeal(@PathVariable("id") Long id, @Valid @RequestBody WhetherMealRequest request) {
+        whetherMealService.saveWhetherMeal(id, request.getMealType());
+    }
 }
